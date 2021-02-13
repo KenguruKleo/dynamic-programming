0d604c8ec26497b0e04bc11c2ce080fe4aa522bd
Konstantin Yemelianov <k.yemelianov@gmail.com>
2021-02-13 20:11:56 +0100

    fibonachi with tabulation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dynamic-programming
+
+source: https://www.youtube.com/watch?v=oBt53YbR9Kk
---
+++ fib.js
@@ -0,0 +1,18 @@
+const fib = n => {
+  const arr = new Array(n + 1).fill(0);
+  arr[0] = 0;
+  arr[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  return arr[n];
+}
+
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
+console.log(fib(5));
+console.log(fib(6));
+console.log(fib(50));
